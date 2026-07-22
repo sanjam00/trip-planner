a87c3a94e647628ed7fb4573bb8a02267235e0ac
Sanaya James <sanaya.j.james@gmail.com>
2026-07-22 14:12:22 -0700

    implemented protectedroute, loginpage, and app.jsx. added tripspage and signuppage but not developed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,26 @@
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import TripsPage from './pages/TripsPage'
+import ProtectedRoute from './components/ProtectedRoute'
+
 function App(){
 
   return(
-    <h1>App</h1>
+    <BrowserRouter>
+    <Routes>
+      < Route path="/login" element={ <LoginPage /> } />
+      < Route path="/signup" element={ <SignupPage /> } />
+      < Route 
+        path="/trips"
+        element={
+          <ProtectedRoute>
+            <TripsPage />
+          </ProtectedRoute>
+        } 
+      />
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import { useNavigate, Link } from 'react-router-dom'
+import { userAuth } from "../context/AuthContext";
+
+
+export default function LoginPage(){
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  // define and import the login function from AuthContext
+  const { login } = userAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+
+    try {
+      await login(username, password);
+      navigate('/trips')
+    } catch(err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Log In</h1>
+
+      {err && <p style={{color: 'red'}}>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" disabled={loading}>
+          {loading ? 'Logging in...' : 'Log In'}
+        </button>
+      </form>
+
+      <p>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+
+    </div>
+  )
+}
