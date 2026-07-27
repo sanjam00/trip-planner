63a05c579d1d370b80a53bf23ae7a3e28a5fdf61
Sanaya James <sanaya.j.james@gmail.com>
2026-07-27 14:10:37 -0700

    added layout/ and moved navbar.jsx and protectedroute.jsx in there. changed imports

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router"
-import "../styles/Navbar.css"
+import "../../styles/Navbar.css"
 
 export default function NavBar() {
 
--- client/src/components/layout/ProtectedRoute.jsx
+++ client/src/components/layout/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 
 import { Navigate } from 'react-router';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 export default function ProtectedRoute({ children }) {
   const { token } = useAuth();
